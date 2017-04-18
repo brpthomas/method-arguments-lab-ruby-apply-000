af1e31a6db54f8d5d3eb3cfbce60726d89e6ce2c
Peter Hoang <pthoang6@gmail.com>
2017-04-18 04:52:03 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+### The `#introduction` Method
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+### The `#introduction_with_language` Method
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
